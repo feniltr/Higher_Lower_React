a90d31b8205e28621c90c0818610dbc1dab88ebb
feniltr <124085597+feniltr@users.noreply.github.com>
2025-04-03 13:20:49 +0530

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const url = "higher-lower-production.up.railway.app";
+const url = "https://higher-lower-production.up.railway.app";
 
 const animateNumber = (start, end, duration, callback) => {
   if (start === null || end === null) return;
