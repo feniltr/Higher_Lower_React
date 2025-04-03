b326328eeda5dcea18c2bbac4554e26b85269a32
feniltr <124085597+feniltr@users.noreply.github.com>
2025-04-03 12:59:41 +0530

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const url = "http://127.0.0.1:8000";
+const url = "https://higher-lower-react.onrender.com";
 
 const animateNumber = (start, end, duration, callback) => {
   if (start === null || end === null) return;
